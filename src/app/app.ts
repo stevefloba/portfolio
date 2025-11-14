5b06434650ba7b0d1630b7fd6b3e39acc135546f
Stefan Flosbach <stefanflosbach@t-online.de>
2025-11-14 19:44:55 +0100

    Seiten- und Componenten-Verlinkungen erstellt. Überflüssigen Code entfernt.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/header/header';
+import { LandingPage } from './landing-page/landing-page';
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, LandingPage, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/landing-page/landing-page.ts
+++ src/app/landing-page/landing-page.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { SeeFirst } from './see-first/see-first';
+import { AboutMe } from './about-me/about-me';
+import { SkillSet } from './skill-set/skill-set';
+import { Projects } from './projects/projects';
+import { ColleaguesThoughts } from './colleagues-thoughts/colleagues-thoughts';
+import { ContactMe } from './contact-me/contact-me';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [SeeFirst, AboutMe, SkillSet, Projects, ColleaguesThoughts, ContactMe],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
